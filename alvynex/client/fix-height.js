79922503ae613e5bf959d4ff66dabe72ade15340
arfa-sk <arfasohail166@gmail.com>
2026-08-02 01:25:06 +0500

    Remove alvynex folder from repository

--- alvynex/client/fix-height.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf8');
-
-html = html.replace('<div role="listitem" class="voices_item fade-in-stagger visible" style="top: 15vh;">', '<div role="listitem" class="voices_item fade-in-stagger visible">');
-html = html.replace('<div role="listitem" class="voices_item fade-in-stagger visible" style="top: 18vh;">', '<div role="listitem" class="voices_item fade-in-stagger visible">');
-html = html.replace('<div role="listitem" class="voices_item fade-in-stagger visible" style="top: 21vh;">', '<div role="listitem" class="voices_item fade-in-stagger visible">');
-
-fs.writeFileSync('index.html', html);
--- alvynex/client/fix3.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf8');
-
-html = html.replace(/<h3>3\+<\/h3>\s*<p>Production Apps<\/p>/g, '<h3>20+</h3>\n<p>Clients Served</p>');
-html = html.replace(/<h3>1\+<\/h3>\s*<p>Years Experience<\/p>/g, '<h3>150k+</h3>\n<p>Views Generated</p>');
-html = html.replace(/<h3>10\+<\/h3>\s*<p>Technologies<\/p>/g, '<h3>6+</h3>\n<p>Industries Served</p>');
-html = html.replace('<image href="assets/testimonial.png"', '<image href="assest/folder.png"');
-
-fs.writeFileSync('index.html', html);
